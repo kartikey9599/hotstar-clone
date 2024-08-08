8c07e96a4963471a82da0d872c44df6dfbab4089
Kartikey <kartikeygupta9599@gmail.com>
2024-08-08 11:51:57 +0530

    done adding data to firebase and fetch it from there to my web application

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/users/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 const listnerMiddleware = createListenerMiddleware({
   serializableCheck: false,
 });
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: (getDefultMiddleware) =>
     getDefultMiddleware().concat(listnerMiddleware.middleware),
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,91 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/users/userSlice";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const username = useSelector(selectUserName);
+  let recommends = [];
+  let newDisney = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    console.log("hello");
+
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        console.log(recommends);
+
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisney = [...newDisney, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisney,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [username]);
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
+    </Container>
+  );
 };
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 9 calc(3.5vw + 5px);
+  padding-left: 10px;
+  padding-right: 10px;
+
+  &::after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default Home;
